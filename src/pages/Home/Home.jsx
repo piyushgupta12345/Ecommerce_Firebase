d07eafb8dab8b995ba05a105354d927d45ed87fc
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-11-07 21:44:50 +0530

    filter component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import HeroSection from '../../components/HeroSection/HeroSection'
+import Filter from '../../components/Filter/Filter'
 
 function Home() {
 
   return (
     <Layout>
       <HeroSection/>
+      <Filter/>
     </Layout>
   )
 }
